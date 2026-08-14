b6ad0aa74f2a5031c9a88a4790f6e3698ea836e3
ummaraali2 <syeda.umm@northeastern.edu>
2026-08-14 16:43:30 -0400

    Fix SSL config to be conditional on NODE_ENV, so CI's local Postgres container works alongside Neon in dev/production

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.NODE_ENV === 'test' ? false : { rejectUnauthorized: false }
 })
 
 module.exports = pool
